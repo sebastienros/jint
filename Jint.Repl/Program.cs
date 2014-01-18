41b4248ae63f8d571407dbc51e1c7d826bb01a91
Sebastien Ros <sebastien.ros@microsoft.com>
2014-01-18 12:27:43 -0800

    Fixing Repl exception on undefined

--- Jint.Repl/Program.cs
+++ Jint.Repl/Program.cs
@@ -25,7 +25,7 @@ namespace Jint.Repl
                 try
                 {
                     var result = engine.GetValue(engine.Execute(input));
-                    var str = engine.Json.Stringify(engine.Json, Arguments.From(result, Undefined.Instance, "  ")).AsString();
+                    var str = TypeConverter.ToString(engine.Json.Stringify(engine.Json, Arguments.From(result, Undefined.Instance, "  ")));
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("=> {0}", str);
                 }
