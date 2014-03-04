3609d007eead2adb8ec62b4b63e3205e556544f0
Sebastien Ros <sebastien.ros@microsoft.com>
2014-03-04 10:49:34 -0800

    Bumping version number

--- Jint/Properties/AssemblyInfo.cs
+++ Jint/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
