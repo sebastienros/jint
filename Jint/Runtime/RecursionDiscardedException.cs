2230e250a149531197648200e636df8a74061bb3
auz34 <andrewzavgorodniy@gmail.com>
2014-07-17 15:00:39 -0700

    additional changes to previous commit

---
+++ Jint/Runtime/RecursionDiscardedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jint.Runtime
+{
+    public class RecursionDiscardedException : Exception 
+    {
+        public RecursionDiscardedException()
+            : base("The recursion is forbidden by script host.")
+        {
+        }
+    }
+}
