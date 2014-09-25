6ab9a74965a074df8ce477098532548277b18551
auz34 <andrewzavgorodniy@gmail.com>
2014-09-24 19:47:50 -0700

    Introduce structures to manage CallStack nicer and faster

---
+++ Jint/Runtime/CallStack/CallStackElementComparer.cs
@@ -0,0 +1,17 @@
+﻿namespace Jint.Runtime.CallStack
+{
+    using System.Collections.Generic;
+
+    public class CallStackElementComparer: IEqualityComparer<CallStackElement>
+    {
+        public bool Equals(CallStackElement x, CallStackElement y)
+        {
+            return x.CallExpression == y.CallExpression || x.Function == y.Function;
+        }
+
+        public int GetHashCode(CallStackElement obj)
+        {
+            return obj.CallExpression.GetHashCode() + obj.Function.GetHashCode();
+        }
+    }
+}
---
+++ Jint/Runtime/CallStack/JintCallStack.cs
@@ -0,0 +1,41 @@
+﻿namespace Jint.Runtime.CallStack
+{
+    using System.Collections.Generic;
+
+    public class JintCallStack
+    {
+        private Stack<CallStackElement> _stack = new Stack<CallStackElement>();
+
+        private Dictionary<CallStackElement, int> _statistics =
+            new Dictionary<CallStackElement, int>(new CallStackElementComparer());
+
+        public int Push(CallStackElement item)
+        {
+            _stack.Push(item);
+            if (_statistics.ContainsKey(item))
+            {
+                return ++_statistics[item];
+            }
+            else
+            {
+                _statistics.Add(item, 0);
+                return 0;
+            }
+        }
+
+        public void Pop()
+        {
+            var item = _stack.Pop();
+            if (_statistics[item] == 0)
+            {
+                _statistics.Remove(item);
+            }
+            else
+            {
+                _statistics[item]--;
+            }
+        }
+
+        // TODO printing Call Stack might become useful for debugging purposes
+    }
+}
