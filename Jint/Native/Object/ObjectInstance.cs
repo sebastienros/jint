5fb341eb3ba164939072ce37f7c295d7692a44ab
Sebastien Ros <sebastien.ros@microsoft.com>
2015-06-24 23:55:14 -0700

    Checking also type before shortcutting assignment

--- Jint/Native/Object/ObjectInstance.cs
+++ Jint/Native/Object/ObjectInstance.cs
@@ -401,7 +401,7 @@ namespace Jint.Native.Object
 
                 (!current.Get.HasValue && !desc.Get.HasValue) || (current.Get.HasValue && desc.Get.HasValue && ExpressionInterpreter.SameValue(current.Get.Value, desc.Get.Value)) &&
                 (!current.Set.HasValue && !desc.Set.HasValue) || (current.Set.HasValue && desc.Set.HasValue && ExpressionInterpreter.SameValue(current.Set.Value, desc.Set.Value)) &&
-                (!current.Value.HasValue && !desc.Value.HasValue) || (current.Value.HasValue && desc.Value.HasValue && ExpressionInterpreter.SameValue(current.Value.Value, desc.Value.Value))
+                (!current.Value.HasValue && !desc.Value.HasValue) || (current.Value.HasValue && desc.Value.HasValue && (current.Value.Value.Type == desc.Value.Value.Type) && ExpressionInterpreter.SameValue(current.Value.Value, desc.Value.Value))
             ) {
                 return true;
             }
