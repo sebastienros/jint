63df467a596e079a9b49f8677d5eb1044445f4b3
Maciej Nowak <maciejt.nowak@gmail.com>
2015-09-30 14:03:46 +0200

    Failing test showing that it is not possible to catch CLR exceptions

---
+++ Jint.Tests/CatchingClrExceptionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jint.Tests
+{
+    public class CatchingClrExceptionsTest
+    {
+        [Fact]
+        public void ItShouldBePossibleToCatchClrExceptionsInJavaScript()
+        {
+            var engine = new Engine();
+            engine.SetValue("error", new Action(() => { throw new ArgumentOutOfRangeException("x"); }));
+
+            engine.Execute(@"
+function run(){
+    try {
+        error();
+        return false;
+    } 
+    catch(e) {
+        return true;
+    }
+}
+");
+
+            var result = engine.Invoke("run");
+
+            Assert.Equal(result.AsBoolean(), true);
+        }
+    }
+}
