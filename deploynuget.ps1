0212f28be848a5ded7556ea3686857656c841478
Sebastien Ros <sebastienros@gmail.com>
2016-10-17 08:35:29 -0700

    Removing nuget binaries

--- deploynuget.ps1
+++ deploynuget.ps1
@@ -1,3 +1,9 @@
+if(![System.IO.File]::Exists("./.nuget/nuget.exe")){
+    # file with path $path doesn't exist
+}
+
+Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "./.nuget/nuget.exe"
+
 Remove-Item .\artifacts -Force -Recurse
-dotnet pack .\jint\project.json -c Release -o .\artifacts
+dotnet pack .\src\Esprima\project.json -c Release -o .\artifacts
 .\.nuget\nuget.exe push .\artifacts\*.nupkg -Source nuget.org -ApiKey $nugetApiKey
