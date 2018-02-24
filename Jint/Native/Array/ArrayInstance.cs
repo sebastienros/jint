5063db0fd43ea897977b4b9f9fdf1710007c65fa
pranay <pranay.rauthu@gmail.com>
2018-02-24 09:55:07 +0530

    Added Enumerator for ArrayInstance

--- Jint/Native/Array/ArrayInstance.cs
+++ Jint/Native/Array/ArrayInstance.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Jint.Native.Object;
 using Jint.Runtime;
@@ -9,7 +10,7 @@ using TypeConverter = Jint.Runtime.TypeConverter;
 
 namespace Jint.Native.Array
 {
-    public class ArrayInstance : ObjectInstance
+    public class ArrayInstance : ObjectInstance, IEnumerable<JsValue>
     {
         private readonly Engine _engine;
 
@@ -593,6 +594,22 @@ namespace Jint.Native.Array
                 System.Array.Copy(_dense, newArray, _dense.Length);
                 _dense = newArray;
             }
+        }
+
+        public IEnumerator<JsValue> GetEnumerator()
+        {
+            for (uint i = 0; i < GetLength(); i++)
+            {
+                if (TryGetValue(i, out JsValue outValue))
+                {
+                    yield return outValue;
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
