3484a83f1bf637a40a4c8fee6618d2fc0103d451
Marko Lahma <marko.lahma@gmail.com>
2018-08-13 21:11:29 +0300

    add Test262 test cases

---
+++ Jint.Tests.Test262/test/built-ins/Proxy/apply/trap-is-not-callable.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 9.5.13
+description: >
+    Throws if trap is not callable.
+features: [Proxy]
+---*/
+
+var p = new Proxy(function() {}, {
+  apply: {}
+});
+
+assert.throws(TypeError, function() {
+  p();
+});
