19c0193e839832403d4f97434c61620ecb45e370
Marko Lahma <marko.lahma@gmail.com>
2019-10-05 11:43:56 +0300

    fix date handling by using integers instead of doubles when logical

--- Jint/Native/Date/DateInstance.cs
+++ Jint/Native/Date/DateInstance.cs
@@ -7,10 +7,10 @@ namespace Jint.Native.Date
     public class DateInstance : ObjectInstance
     {
         // Maximum allowed value to prevent DateTime overflow
-        internal static readonly double Max = (DateTime.MaxValue - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+        private static readonly double Max = (DateTime.MaxValue - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
 
         // Minimum allowed value to prevent DateTime overflow
-        internal static readonly double Min = -(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc) - DateTime.MinValue).TotalMilliseconds;
+        private static readonly double Min = -(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc) - DateTime.MinValue).TotalMilliseconds;
 
         public DateInstance(Engine engine)
             : base(engine, objectClass: "Date")
@@ -21,13 +21,10 @@ namespace Jint.Native.Date
         {
             if (double.IsNaN(PrimitiveValue) || PrimitiveValue > Max || PrimitiveValue < Min)
             {
-                ExceptionHelper.ThrowRangeError(Engine);
-                return DateTime.MinValue;
+                return ExceptionHelper.ThrowRangeError<DateTime>(Engine);
             }
-            else
-            {
-                return DateConstructor.Epoch.AddMilliseconds(PrimitiveValue);
-            }
+
+            return DateConstructor.Epoch.AddMilliseconds(PrimitiveValue);
         }
 
         public double PrimitiveValue { get; set; }
