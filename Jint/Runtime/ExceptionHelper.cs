e43290d7e999a6f743363a33d53741b9fe2a1fe0
Sébastien Ros <sebastienros@gmail.com>
2020-03-04 10:45:49 -0800

    Fix possible NRE in CallStackElement construction (#708)

--- Jint/Runtime/ExceptionHelper.cs
+++ Jint/Runtime/ExceptionHelper.cs
@@ -37,7 +37,7 @@ namespace Jint.Runtime
 
         public static void ThrowReferenceError(Engine engine, Reference reference)
         {
-            ThrowReferenceError(engine, (reference?.GetReferencedName()).ToString());
+            ThrowReferenceError(engine, reference?.GetReferencedName()?.ToString());
         }
 
         public static void ThrowReferenceError(Engine engine, string name)
