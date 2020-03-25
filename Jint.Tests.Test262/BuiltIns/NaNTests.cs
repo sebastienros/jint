b7a44c91f68f28da7c31cbecf1726145720de36d
Marko Lahma <marko.lahma@gmail.com>
2020-03-25 19:58:19 +0200

    enable some more test cases

---
+++ Jint.Tests.Test262/BuiltIns/NaNTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Jint.Tests.Test262.BuiltIns
+{
+    public class NaNTests : Test262Test
+    {
+        [Theory(DisplayName = "built-ins\\NaN")]
+        [MemberData(nameof(SourceFiles), "built-ins\\NaN", false)]
+        [MemberData(nameof(SourceFiles), "built-ins\\NaN", true, Skip = "Skipped")]
+        protected void RunTest(SourceFile sourceFile)
+        {
+            RunTestInternal(sourceFile);
+        }
+    }
+}
