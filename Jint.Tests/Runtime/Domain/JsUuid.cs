7a30b5a43e436ddf3eb8214e3e60a6b0b9cd220e
Michael Kriese <michael.kriese@visualon.de>
2020-07-16 16:35:39 +0200

    Add protected function instance constructor

---
+++ Jint.Tests/Runtime/Domain/JsUuid.cs
@@ -0,0 +1,25 @@
+using System;
+using Jint.Native;
+
+namespace Jint.Tests.Runtime.Domain
+{
+    public sealed class JsUuid : JsValue, IEquatable<JsUuid>
+    {
+        internal readonly Guid _value;
+        public static readonly JsUuid Empty = new JsUuid(Guid.Empty);
+
+        public JsUuid(Guid value) : base(Jint.Runtime.Types.String) => _value = value;
+
+        public static implicit operator JsUuid(Guid g) => new JsUuid(g);
+
+        public override bool Equals(JsValue other) => Equals(other as JsUuid);
+
+        public bool Equals(JsUuid other) => other?._value == _value;
+
+        public override int GetHashCode() => _value.GetHashCode();
+
+        public override object ToObject() => _value;
+
+        public override string ToString() => _value.ToString();
+    }
+}
