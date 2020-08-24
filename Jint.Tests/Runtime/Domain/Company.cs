24cda031a81033f49fa70ad0d3c66cc083e4066e
Marko Lahma <marko.lahma@gmail.com>
2020-08-24 19:49:54 +0300

    Prefer indexing and custom properties over indexer property (#773)

--- Jint.Tests/Runtime/Domain/Company.cs
+++ Jint.Tests/Runtime/Domain/Company.cs
@@ -6,7 +6,11 @@ namespace Jint.Tests.Runtime.Domain
     public class Company : ICompany, IComparable<ICompany>
     {
         private string _name;
-        private readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+        private readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>()
+        {
+            {"key", "value"}
+        };
 
         public Company(string name)
         {
@@ -15,15 +19,17 @@ namespace Jint.Tests.Runtime.Domain
 
         string ICompany.Name
         {
-            get { return _name; }
-            set { _name = value; }
+            get => _name;
+            set => _name = value;
         }
 
         string ICompany.this[string key]
         {
-            get { return _dictionary[key]; }
-            set { _dictionary[key] = value; }
+            get => _dictionary[key];
+            set => _dictionary[key] = value;
         }
+
+        public string Item => "item thingie";
 
         int IComparable<ICompany>.CompareTo(ICompany other)
         {
