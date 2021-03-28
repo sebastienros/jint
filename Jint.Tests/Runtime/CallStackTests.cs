70eb6444039603145ac822a2ffd5ba88524227a4
Jither <jimmith@gmail.com>
2021-03-28 08:20:34 +0200

    Tests of CallStack unwinding after caught exception (and quick patch) (#853)
    * (Currently failing) tests of callstack unwinding after caught exception
    
    * No need to not indent the JS in tests
    
    * Quick patch for the issue (not intended as a final solution)

---
+++ Jint.Tests/Runtime/CallStackTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jint.Tests.Runtime
+{
+    public class CallStackTests
+    {
+        [Fact]
+        public void ShouldUnwindAfterCaughtException()
+        {
+            var engine = new Engine();
+            engine.Execute(@"
+                function thrower()
+                {
+                    throw new Error('test');
+                }
+
+                try
+                {
+                    thrower();
+                }
+                catch (error)
+                {
+                }
+                "
+            );
+            Assert.Equal(0, engine.CallStack.Count);
+        }
+
+        [Fact]
+        public void ShouldUnwindAfterCaughtExceptionNested()
+        {
+            var engine = new Engine();
+            engine.Execute(@"
+                function thrower2()
+                {
+                    throw new Error('test');
+                }
+
+                function thrower1()
+                {
+                    thrower2();
+                }
+
+                try
+                {
+                    thrower1();
+                }
+                catch (error)
+                {
+                }
+            ");
+            Assert.Equal(0, engine.CallStack.Count);
+        }
+    }
+}
