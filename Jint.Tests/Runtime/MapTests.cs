2631405dbbd143e2c002016ed1c0ce05c4aacabf
Marko Lahma <marko.lahma@gmail.com>
2021-05-13 18:02:12 +0300

    Fix Map and Set call validation (#895)

---
+++ Jint.Tests/Runtime/MapTests.cs
@@ -0,0 +1,15 @@
+using Jint.Runtime;
+using Xunit;
+
+namespace Jint.Tests.Runtime
+{
+    public class MapTests
+    {
+        [Fact]
+        public void ShouldThrowWhenCalledWithoutNew()
+        {
+            var e = Assert.Throws<JavaScriptException>(() => new Engine().Execute("const m = new Map(); Map.call(m,[]);"));
+            Assert.Equal("Constructor Map requires 'new'", e.Message);
+        }
+    }
+}
