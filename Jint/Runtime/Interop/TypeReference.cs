43d63be7a3e9d0f154036ce7c88528fa94702e11
Marko Lahma <marko.lahma@gmail.com>
2022-01-12 07:13:23 +0200

    Allow JS class to extend CLR type (#1049)

--- Jint/Runtime/Interop/TypeReference.cs
+++ Jint/Runtime/Interop/TypeReference.cs
@@ -50,24 +50,35 @@ namespace Jint.Runtime.Interop
 
         private ObjectInstance Construct(JsValue[] arguments)
         {
+            ObjectInstance result = null;
             if (arguments.Length == 0 && ReferenceType.IsValueType)
             {
                 var instance = Activator.CreateInstance(ReferenceType);
-                var result = TypeConverter.ToObject(_realm, FromObject(Engine, instance));
+                result = TypeConverter.ToObject(_realm, FromObject(Engine, instance));
+            }
+            else
+            {
+                var constructors = _constructorCache.GetOrAdd(
+                    ReferenceType,
+                    t => MethodDescriptor.Build(t.GetConstructors(BindingFlags.Public | BindingFlags.Instance)));
 
-                return result;
+                foreach (var (method, _, _) in TypeConverter.FindBestMatch(_engine, constructors, _ => arguments))
+                {
+                    var retVal = method.Call(Engine, null, arguments);
+                    result = TypeConverter.ToObject(_realm, retVal);
+
+                    // todo: cache method info
+                    break;
+                }
             }
 
-            var constructors = _constructorCache.GetOrAdd(
-                ReferenceType,
-                t => MethodDescriptor.Build(t.GetConstructors(BindingFlags.Public | BindingFlags.Instance)));
-
-            foreach (var (method, _, _) in TypeConverter.FindBestMatch(_engine, constructors, _ => arguments))
+            if (result is not null)
             {
-                var retVal = method.Call(Engine, null, arguments);
-                var result = TypeConverter.ToObject(_realm, retVal);
-
-                // todo: cache method info
+                if (result is ObjectWrapper objectWrapper)
+                {
+                    // allow class extension
+                    objectWrapper._allowAddingProperties = true;
+                }
 
                 return result;
             }
@@ -127,11 +138,15 @@ namespace Jint.Runtime.Interop
             if (_properties?.TryGetValue(key, out descriptor) != true)
             {
                 descriptor = CreatePropertyDescriptor(key);
-                _properties ??= new PropertyDictionary();
-                _properties[key] = descriptor;
+                if (!ReferenceEquals(descriptor, PropertyDescriptor.Undefined))
+                {
+                    _properties ??= new PropertyDictionary();
+                    _properties[key] = descriptor;
+                    return descriptor;
+                }
             }
 
-            return descriptor;
+            return base.GetOwnProperty(property);
         }
 
         private PropertyDescriptor CreatePropertyDescriptor(string name)
