7f273eb3a79ebe305d51f9fa6f3e937fa229e364
Christian Rondeau <christian.rondeau@gmail.com>
2022-01-18 11:19:22 -0500

    Export default

--- Jint/Runtime/Interpreter/Statements/JintExportDefaultDeclarationStatement.cs
+++ Jint/Runtime/Interpreter/Statements/JintExportDefaultDeclarationStatement.cs
@@ -1,18 +1,35 @@
 ﻿#nullable enable
 
-using System;
 using Esprima.Ast;
+using Jint.Native;
+using Jint.Runtime.Environments;
+using Jint.Runtime.Interpreter.Expressions;
+using Jint.Runtime.Modules;
+using Jint.Runtime.References;
 
 namespace Jint.Runtime.Interpreter.Statements;
 
 internal sealed class JintExportDefaultDeclarationStatement : JintExportDeclarationStatement<ExportDefaultDeclaration>
 {
+    private JintExpression? _init;
+
     public JintExportDefaultDeclarationStatement(ExportDefaultDeclaration statement) : base(statement)
     {
     }
 
     protected override void Initialize(EvaluationContext context)
     {
-        throw new NotImplementedException();
+        _init = JintExpression.Build(context.Engine, (Expression) _statement.Declaration);
+    }
+
+    protected override Completion ExecuteInternal(EvaluationContext context)
+    {
+        var module = context.Engine.GetActiveScriptOrModule() as JsModule;
+        if (module == null) throw new JavaScriptException("Export can only be used in a module");
+
+        var completion = _init?.GetValue(context) ?? Completion.Empty();
+        module._environment.CreateImmutableBindingAndInitialize("*default*", true, completion.Value);
+
+        return Completion.Empty();
     }
 }
