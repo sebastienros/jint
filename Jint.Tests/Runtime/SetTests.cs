002b6b2d032a9fe76e3ba75ebacde325af1838b7
Gijsbert ter Horst <Ghostbird@users.noreply.github.com>
2022-02-10 13:08:36 +0100

    External serialisation of set fails

--- Jint.Tests/Runtime/SetTests.cs
+++ Jint.Tests/Runtime/SetTests.cs
@@ -11,5 +11,14 @@ namespace Jint.Tests.Runtime
             var e = Assert.Throws<JavaScriptException>(() => new Engine().Execute("const m = new Set(); Set.call(m,[]);"));
             Assert.Equal("Constructor Set requires 'new'", e.Message);
         }
+
+        [Fact]
+        public void SerializableEvaluated()
+        {
+            var internally = new Engine().Evaluate("JSON.stringify(new Set())");
+            Assert.Equal("{}", internally);
+            var externally = JsonConvert.SerializeObject(new Engine().Evaluate("new Set()"));
+            Assert.Equal("{}", externally);
+        }
     }
 }
