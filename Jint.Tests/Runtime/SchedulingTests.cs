9c67783708054c2191552222776edab1a818d614
Sebastian <sebastian@squidex.io>
2022-02-11 20:14:30 +0100

    Another test

--- Jint.Tests/Runtime/SchedulingTests.cs
+++ Jint.Tests/Runtime/SchedulingTests.cs
@@ -37,6 +37,36 @@ namespace Jint.Tests.Runtime
 
             Assert.Equal("Hello World", ctx.Result);
         }
+        [Fact]
+        public async Task CanRunNestedAsyncCode()
+        {
+            var engine = new Engine();
+
+            var ctx = new Context();
+
+            engine.SetValue("ctx", ctx);
+            engine.SetValue("setTimeout", new Action<Action, int>(async (callback, delay) =>
+            {
+                using (var task = engine.CreateTask())
+                {
+                    await Task.Delay(delay);
+
+                    task.Invoke(callback);
+                }
+            }));
+
+            var result = await engine.EvaluateAsync(@"
+                setTimeout(function () {
+                    setTimeout(function () {
+                        setTimeout(function () {
+                            ctx.Result = 'Hello World';
+                        }, 100);
+                    }, 100);
+                }, 100);
+            ");
+
+            Assert.Equal("Hello World", ctx.Result);
+        }
 
         [Fact]
         public async Task CanRunSyncCode()
