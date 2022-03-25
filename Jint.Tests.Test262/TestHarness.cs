650c0cbc4efc97d35e77f6cbdc002d5e6231e811
Marko Lahma <marko.lahma@gmail.com>
2022-03-25 19:08:04 +0200

    Date related fixes and improvements (#1107)

--- Jint.Tests.Test262/TestHarness.cs
+++ Jint.Tests.Test262/TestHarness.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -13,18 +12,6 @@ public partial class TestHarness
 {
     private static partial Task InitializeCustomState()
     {
-        // NOTE: The Date tests in test262 assume the local timezone is Pacific Standard Time
-        try
-        {
-            State.TimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
-        }
-        catch (TimeZoneNotFoundException)
-        {
-            // https://stackoverflow.com/questions/47848111/how-should-i-fetch-timezoneinfo-in-a-platform-agnostic-way
-            // should be natively supported soon https://github.com/dotnet/runtime/issues/18644
-            State.TimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles");
-        }
-
         foreach (var file in State.HarnessFiles)
         {
             var source = file.Program;
