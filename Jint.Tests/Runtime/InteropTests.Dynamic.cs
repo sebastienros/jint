cb4e73de96bf21cd63f948978a5b19c8da0a5884
Marko Lahma <marko.lahma@gmail.com>
2022-07-15 08:38:29 +0300

    Create sample how to JSON.stringify CLR types (#1226)

--- Jint.Tests/Runtime/InteropTests.Dynamic.cs
+++ Jint.Tests/Runtime/InteropTests.Dynamic.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
 using Xunit;
 
 namespace Jint.Tests.Runtime
@@ -32,34 +31,6 @@ namespace Jint.Tests.Runtime
             Assert.Equal("abc", _engine.Evaluate("parent.child.item.title"));
         }
 
-
-        [Fact]
-        public void EngineShouldStringifyAnExpandoObjectCorrectly()
-        {
-            var engine = new Engine();
-
-            dynamic expando = new ExpandoObject();
-            expando.foo = 5;
-            expando.bar = "A string";
-            engine.SetValue(nameof(expando), expando);
-
-            var result = engine.Evaluate($"JSON.stringify({nameof(expando)})").AsString();
-            Assert.Equal("{\"foo\":5,\"bar\":\"A string\"}", result);
-        }
-
-        [Fact]
-        public void EngineShouldStringifyAnExpandoObjectWithValuesCorrectly()
-        {
-            // https://github.com/sebastienros/jint/issues/995
-            var engine = new Engine();
-
-            dynamic expando = new ExpandoObject();
-            expando.Values = 1;
-            engine.SetValue("expando", expando);
-
-            Assert.Equal("{\"Values\":1}", engine.Evaluate($"JSON.stringify(expando)").AsString());
-        }
-
         [Fact]
         public void ShouldForOfOnExpandoObject()
         {
@@ -89,46 +60,6 @@ namespace Jint.Tests.Runtime
 
             Assert.Equal(1d, dic["a"]);
             Assert.Equal("foo", dic["b"]);
-        }
-
-        [Fact]
-        public void EngineShouldStringifyAnJObjectArrayWithValuesCorrectly()
-        {
-            //https://github.com/OrchardCMS/OrchardCore/issues/10648
-            var engine = new Engine();
-            var queryResults = new List<dynamic>();
-            queryResults.Add(new { Text = "Text1", Value = 1 });
-            queryResults.Add(new { Text = "Text2", Value = 2 });
-
-            engine.SetValue("testSubject", queryResults.ToArray());
-            var fromEngine2 = engine.Evaluate("return JSON.stringify(testSubject);");
-            var result2 = fromEngine2.ToString();
-            Assert.Equal("[{\"Text\":\"Text1\",\"Value\":1},{\"Text\":\"Text2\",\"Value\":2}]", result2);
-        }
-
-        [Fact]
-        public void EngineShouldStringifyDynamicObjectListWithValuesCorrectly()
-        {
-            var engine = new Engine();
-            var source = new dynamic[] { new { Text = "Text1", Value = 1 }, new { Text = "Text2", Value = 2 } };
-
-            var objects = source.ToList();
-            engine.SetValue("testSubject", objects);
-            var fromEngine = engine.Evaluate("return JSON.stringify(testSubject);");
-            var result = fromEngine.ToString();
-            Assert.Equal("[{\"Text\":\"Text1\",\"Value\":1},{\"Text\":\"Text2\",\"Value\":2}]", result);
-        }
-
-        [Fact]
-        public void EngineShouldStringifyDynamicObjectArrayWithValuesCorrectly()
-        {
-            var engine = new Engine();
-            var source = new dynamic[] { new { Text = "Text1", Value = 1 }, new { Text = "Text2", Value = 2 } };
-
-            engine.SetValue("testSubject", source.AsEnumerable());
-            var fromEngine = engine.Evaluate("return JSON.stringify(testSubject);");
-            var result = fromEngine.ToString();
-            Assert.Equal("[{\"Text\":\"Text1\",\"Value\":1},{\"Text\":\"Text2\",\"Value\":2}]", result);
         }
 
         [Fact]
