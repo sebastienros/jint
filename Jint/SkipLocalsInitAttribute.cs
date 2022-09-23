42be828d93368c20fdd19fa3693a3580d451d84f
Marko Lahma <marko.lahma@gmail.com>
2022-09-23 20:18:47 +0300

    Add SkipLocalsInit attribute
    * remove IsExternalInit NuGet reference and use own

---
+++ Jint/SkipLocalsInitAttribute.cs
@@ -0,0 +1,15 @@
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(
+    AttributeTargets.Module
+    | AttributeTargets.Class
+    | AttributeTargets.Struct
+    | AttributeTargets.Interface
+    | AttributeTargets.Constructor
+    | AttributeTargets.Method
+    | AttributeTargets.Property
+    | AttributeTargets.Event, Inherited = false)]
+internal sealed class SkipLocalsInitAttribute : Attribute
+{
+    public SkipLocalsInitAttribute() { }
+}
