3e3188784c591413b89dc751c4a4ba8d5a4de21f
ENZ <martin.enzelsberger@combeenation.com>
2022-11-08 13:29:38 +0100

    Add PublicInterface test for CancellationConstraint.Reset, update README
    The example usage of CancellationConstraint demonstrated in the README
    doesn't work anymore because the constructor was made internal.
    
    A dummy CancellationToken must be provided when creating the constraint
    if one intends to pass the actual one via Reset().
    `default` doesn't work though, as the constraint won't be created then.
    
    #1345

---
+++ Jint.Tests.PublicInterface/ConstraintUsageTests.cs
@@ -0,0 +1,42 @@
+﻿using Jint.Constraints;
+using Jint.Runtime;
+
+namespace Jint.Tests.PublicInterface
+{
+    public class ConstraintUsageTests
+    {
+        [Fact]
+        public void CanFindAndResetCancellationConstraint()
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+            var engine = new Engine(new Options().CancellationToken(cts.Token));
+
+            // expect constraint to abort execution due to timeout
+            Assert.Throws<ExecutionCanceledException>(WaitAndCompute);
+
+            // ensure constraint can be obtained publicly
+            var cancellationConstraint = engine.FindConstraint<CancellationConstraint>();
+            Assert.NotNull(cancellationConstraint);
+
+            // reset constraint, expect computation to finish this time
+            using var cts2 = new CancellationTokenSource(TimeSpan.FromMilliseconds(500));
+            cancellationConstraint.Reset(cts2.Token);
+            Assert.Equal("done", WaitAndCompute());
+
+            string WaitAndCompute()
+            {
+                var result = engine.Evaluate(@"
+                    function sleep(millisecondsTimeout) {
+                        var totalMilliseconds = new Date().getTime() + millisecondsTimeout;
+
+                        while (new Date() < totalMilliseconds) { }
+                    }
+
+                    sleep(200);
+                    return 'done';
+                ");
+                return result.AsString();
+            }
+        }
+    }
+}
