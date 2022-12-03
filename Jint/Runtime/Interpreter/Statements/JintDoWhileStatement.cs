9f344bff405106c175a182c1dd24af33d8b2bd93
Marko Lahma <marko.lahma@gmail.com>
2022-12-03 08:50:36 +0200

    Fix if behavior to always return undefined

--- Jint/Runtime/Interpreter/Statements/JintDoWhileStatement.cs
+++ Jint/Runtime/Interpreter/Statements/JintDoWhileStatement.cs
@@ -2,63 +2,62 @@ using Esprima.Ast;
 using Jint.Native;
 using Jint.Runtime.Interpreter.Expressions;
 
-namespace Jint.Runtime.Interpreter.Statements
+namespace Jint.Runtime.Interpreter.Statements;
+
+/// <summary>
+/// http://www.ecma-international.org/ecma-262/5.1/#sec-12.6.1
+/// </summary>
+internal sealed class JintDoWhileStatement : JintStatement<DoWhileStatement>
 {
-    /// <summary>
-    /// http://www.ecma-international.org/ecma-262/5.1/#sec-12.6.1
-    /// </summary>
-    internal sealed class JintDoWhileStatement : JintStatement<DoWhileStatement>
+    private ProbablyBlockStatement _body;
+    private string? _labelSetName;
+    private JintExpression _test = null!;
+
+    public JintDoWhileStatement(DoWhileStatement statement) : base(statement)
     {
-        private ProbablyBlockStatement _body;
-        private string? _labelSetName;
-        private JintExpression _test = null!;
+    }
 
-        public JintDoWhileStatement(DoWhileStatement statement) : base(statement)
+    protected override void Initialize(EvaluationContext context)
+    {
+        _body = new ProbablyBlockStatement(_statement.Body);
+        _test = JintExpression.Build(_statement.Test);
+        _labelSetName = _statement.LabelSet?.Name;
+    }
+
+    protected override Completion ExecuteInternal(EvaluationContext context)
+    {
+        JsValue v = JsValue.Undefined;
+        bool iterating;
+
+        do
         {
-        }
-
-        protected override void Initialize(EvaluationContext context)
-        {
-            _body = new ProbablyBlockStatement(_statement.Body);
-            _test = JintExpression.Build(_statement.Test);
-            _labelSetName = _statement.LabelSet?.Name;
-        }
-
-        protected override Completion ExecuteInternal(EvaluationContext context)
-        {
-            JsValue v = JsValue.Undefined;
-            bool iterating;
-
-            do
+            var completion = _body.Execute(context);
+            if (!ReferenceEquals(completion.Value, null))
             {
-                var completion = _body.Execute(context);
-                if (!ReferenceEquals(completion.Value, null))
+                v = completion.Value;
+            }
+
+            if (completion.Type != CompletionType.Continue || context.Target != _labelSetName)
+            {
+                if (completion.Type == CompletionType.Break && (context.Target == null || context.Target == _labelSetName))
                 {
-                    v = completion.Value;
+                    return new Completion(CompletionType.Normal, v, _statement);
                 }
 
-                if (completion.Type != CompletionType.Continue || context.Target != _labelSetName)
+                if (completion.Type != CompletionType.Normal)
                 {
-                    if (completion.Type == CompletionType.Break && (context.Target == null || context.Target == _labelSetName))
-                    {
-                        return new Completion(CompletionType.Normal, v, _statement);
-                    }
-
-                    if (completion.Type != CompletionType.Normal)
-                    {
-                        return completion;
-                    }
+                    return completion;
                 }
+            }
 
-                if (context.DebugMode)
-                {
-                    context.Engine.DebugHandler.OnStep(_test._expression);
-                }
+            if (context.DebugMode)
+            {
+                context.Engine.DebugHandler.OnStep(_test._expression);
+            }
 
-                iterating = TypeConverter.ToBoolean(_test.GetValue(context));
-            } while (iterating);
+            iterating = TypeConverter.ToBoolean(_test.GetValue(context));
+        } while (iterating);
 
-            return new Completion(CompletionType.Normal, v, ((JintStatement) this)._statement);
-        }
+        return new Completion(CompletionType.Normal, v, ((JintStatement) this)._statement);
     }
 }
--- Jint/Runtime/Interpreter/Statements/JintIfStatement.cs
+++ Jint/Runtime/Interpreter/Statements/JintIfStatement.cs
@@ -1,42 +1,38 @@
 using Esprima.Ast;
+using Jint.Native;
 using Jint.Runtime.Interpreter.Expressions;
 
-namespace Jint.Runtime.Interpreter.Statements
+namespace Jint.Runtime.Interpreter.Statements;
+
+internal sealed class JintIfStatement : JintStatement<IfStatement>
 {
-    internal sealed class JintIfStatement : JintStatement<IfStatement>
+    private ProbablyBlockStatement _statementConsequent;
+    private JintExpression _test = null!;
+    private ProbablyBlockStatement? _alternate;
+
+    public JintIfStatement(IfStatement statement) : base(statement)
     {
-        private ProbablyBlockStatement _statementConsequent;
-        private JintExpression _test = null!;
-        private ProbablyBlockStatement? _alternate;
+    }
 
-        public JintIfStatement(IfStatement statement) : base(statement)
+    protected override void Initialize(EvaluationContext context)
+    {
+        _statementConsequent = new ProbablyBlockStatement(_statement.Consequent);
+        _test = JintExpression.Build(_statement.Test);
+        _alternate = _statement.Alternate != null ? new ProbablyBlockStatement(_statement.Alternate) : null;
+    }
+
+    protected override Completion ExecuteInternal(EvaluationContext context)
+    {
+        Completion result = default;
+        if (TypeConverter.ToBoolean(_test.GetValue(context)))
         {
+            result = _statementConsequent.Execute(context);
+        }
+        else if (_alternate != null)
+        {
+            result = _alternate.Value.Execute(context);
         }
 
-        protected override void Initialize(EvaluationContext context)
-        {
-            _statementConsequent = new ProbablyBlockStatement(_statement.Consequent);
-            _test = JintExpression.Build(_statement.Test);
-            _alternate = _statement.Alternate != null ? new ProbablyBlockStatement(_statement.Alternate) : null;
-        }
-
-        protected override Completion ExecuteInternal(EvaluationContext context)
-        {
-            Completion result;
-            if (TypeConverter.ToBoolean(_test.GetValue(context)))
-            {
-                result = _statementConsequent.Execute(context);
-            }
-            else if (_alternate != null)
-            {
-                result = _alternate.Value.Execute(context);
-            }
-            else
-            {
-                result = new Completion(CompletionType.Normal, null!, _statement);
-            }
-
-            return result;
-        }
+        return result.UpdateEmpty(JsValue.Undefined);
     }
 }
