8a033588cec74c1e590d165f912c7bb0f3f294c4
Marko Lahma <marko.lahma@gmail.com>
2023-02-23 08:36:35 +0200

    Upgrade to Esprima 3.0.0-rc-01 (#1464)

---
+++ Jint/Messages.cs
@@ -0,0 +1,9 @@
+namespace Jint;
+
+/// <summary>
+/// Helper to map Esprima error messages.
+/// </summary>
+internal static class Messages
+{
+    public const string InvalidLHSInAssignment = "Invalid left-hand side in assignment";
+}
