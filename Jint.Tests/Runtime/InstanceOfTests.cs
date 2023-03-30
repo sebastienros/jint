c7425561a74cdff37a8e6d94f00b0e3940030c11
Miguel Fernández Corral <mfcorral@gmail.com>
2023-03-30 03:19:26 +0200

    CLR inheritance chain

--- Jint.Tests/Runtime/InstanceOfTests.cs
+++ Jint.Tests/Runtime/InstanceOfTests.cs
@@ -7,15 +7,26 @@ public class InstanceOfTests
     [Fact]
     public void ShouldSupportInheritanceChainUnderInterop()
     {
-        var a = new A();
-        var b = new B();
-        var c = new C();
-
         var engine = new Engine();
 
         engine.SetValue("A", TypeReference.CreateTypeReference(engine, typeof(A)));
+        engine.SetValue("AToo", TypeReference.CreateTypeReference(engine, typeof(A)));
         engine.SetValue("B", TypeReference.CreateTypeReference(engine, typeof(B)));
         engine.SetValue("C", TypeReference.CreateTypeReference(engine, typeof(C)));
+
+        Assert.True(engine.Evaluate("A == A").AsBoolean());
+        Assert.True(engine.Evaluate("A === A").AsBoolean());
+        Assert.True(engine.Evaluate("A == AToo").AsBoolean());
+        Assert.True(engine.Evaluate("A === AToo").AsBoolean());
+
+        Assert.True(engine.Evaluate("B.prototype instanceof A").AsBoolean());
+        Assert.False(engine.Evaluate("A.prototype instanceof B").AsBoolean());
+        Assert.True(engine.Evaluate("C.prototype instanceof A").AsBoolean());
+        Assert.True(engine.Evaluate("C.prototype instanceof B").AsBoolean());
+
+        var a = new A();
+        var b = new B();
+        var c = new C();
 
         engine.SetValue("a", a);
         engine.SetValue("b", b);
@@ -32,6 +43,18 @@ public class InstanceOfTests
         Assert.True(engine.Evaluate("c instanceof A").AsBoolean());
         Assert.True(engine.Evaluate("c instanceof B").AsBoolean());
         Assert.True(engine.Evaluate("c instanceof C").AsBoolean());
+
+        Assert.True(engine.Evaluate("new A() instanceof A").AsBoolean());
+        Assert.False(engine.Evaluate("new A() instanceof B").AsBoolean());
+        Assert.False(engine.Evaluate("new A() instanceof C").AsBoolean());
+
+        Assert.True(engine.Evaluate("new B() instanceof A").AsBoolean());
+        Assert.True(engine.Evaluate("new B() instanceof B").AsBoolean());
+        Assert.False(engine.Evaluate("new B() instanceof C").AsBoolean());
+
+        Assert.True(engine.Evaluate("new C() instanceof A").AsBoolean());
+        Assert.True(engine.Evaluate("new C() instanceof B").AsBoolean());
+        Assert.True(engine.Evaluate("new C() instanceof C").AsBoolean());
     }
 
     public class A { }
---
+++ Jint/Runtime/Interop/TypeReferencePrototypeDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using Jint.Native.Object;
+
+namespace Jint.Runtime.Interop
+{
+    internal sealed class TypeReferencePrototypeDescriptor : ObjectInstance
+    {
+        private readonly TypeReference _typeReference;
+
+        public TypeReferencePrototypeDescriptor(Engine engine, TypeReference typeReference) : base(engine)
+        {
+            _typeReference = typeReference;
+            _prototype = engine.Realm.Intrinsics.Object.PrototypeObject;
+        }
+
+        public TypeReference TypeReference => _typeReference;
+    }
+
+}
