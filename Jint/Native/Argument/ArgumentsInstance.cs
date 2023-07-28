a41a0e3c67526e2b8cc68c37050055280438a97a
Marko Lahma <marko.lahma@gmail.com>
2023-07-28 22:07:01 +0300

    Make ArgumentsInstance internal (#1594)

--- Jint/Native/Argument/ArgumentsInstance.cs
+++ Jint/Native/Argument/ArgumentsInstance.cs
@@ -13,7 +13,7 @@ namespace Jint.Native.Argument
     /// <summary>
     /// http://www.ecma-international.org/ecma-262/5.1/#sec-10.6
     /// </summary>
-    public sealed class ArgumentsInstance : ObjectInstance
+    internal sealed class ArgumentsInstance : ObjectInstance
     {
         // cache property container for array iteration for less allocations
         private static readonly ThreadLocal<HashSet<string>> _mappedNamed = new(() => new HashSet<string>());
