c905f53c998ba7723e41a0c18e56268cfce5ad6a
Umut Akkaya <Xicy@users.noreply.github.com>
2023-10-12 07:58:18 +0300

    Add experimental support for Task to Promise conversion (#1567)
    This commit fixes an issue where an `async Task` (be it a method or a delegate)
    would end up being marshalled directly to JS, giving a `Task<VoidTaskResult>`
    to the user, instead of `undefined`, which is what is returned for
    "synchronous tasks", i.e. any Task-returning invokable function that does
    not generate an async state machine of its own (that is to say, any function
    that returns `Task`, not `async Task`.
    
    This commit fixes the issue by checking if a Task's result is equal to
    `VoidTaskResult`, which is an internal type used by the runtime to indicate
    a void-returning Task, such as that from an `async Task` method/delegate
    
    ---------
    
    Co-authored-by: Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>

--- Jint.Tests/Runtime/AwaitTests.cs
+++ Jint.Tests/Runtime/AwaitTests.cs
@@ -10,4 +10,72 @@ public class AsyncTests
         result = result.UnwrapIfPromise();
         Assert.Equal("1", result);
     }
+
+    [Fact]
+    public void ShouldTaskConvertedToPromiseInJS()
+    {
+        Engine engine = new();
+        engine.SetValue("callable", Callable);
+        var result = engine.Evaluate("callable().then(x=>x*2)");
+        result = result.UnwrapIfPromise();
+        Assert.Equal(2, result);
+
+        static async Task<int> Callable()
+        {
+            await Task.Delay(10);
+            Assert.True(true);
+            return 1;
+        }
+    }
+
+    [Fact]
+    public void ShouldTaskCatchWhenCancelled()
+    {
+        Engine engine = new();
+        CancellationTokenSource cancel = new();
+        cancel.Cancel();
+        engine.SetValue("token", cancel.Token);
+        engine.SetValue("callable", Callable);
+        engine.SetValue("assert", new Action<bool>(Assert.True));
+        var result = engine.Evaluate("callable(token).then(_ => assert(false)).catch(_ => assert(true))");
+        result = result.UnwrapIfPromise();
+        static async Task Callable(CancellationToken token)
+        {
+            await Task.FromCanceled(token);
+        }
+    }
+
+    [Fact]
+    public void ShouldTaskCatchWhenThrowError()
+    {
+        Engine engine = new();
+        engine.SetValue("callable", Callable);
+        engine.SetValue("assert", new Action<bool>(Assert.True));
+        var result = engine.Evaluate("callable().then(_ => assert(false)).catch(_ => assert(true))");
+
+        static async Task Callable()
+        {
+            await Task.Delay(10);
+            throw new Exception();
+        }
+    }
+
+    [Fact]
+    public void ShouldTaskAwaitCurrentStack()
+    {
+        //https://github.com/sebastienros/jint/issues/514#issuecomment-1507127509
+        Engine engine = new();
+        string log = "";
+        engine.SetValue("myAsyncMethod", new Func<Task>(async () =>
+        {
+            await Task.Delay(1000);
+            log += "1";
+        }));
+        engine.SetValue("myAsyncMethod2", new Action(() =>
+        {
+            log += "2";
+        }));
+        engine.Execute("async function hello() {await myAsyncMethod();myAsyncMethod2();} hello();");
+        Assert.Equal("12", log);
+    }
 }
