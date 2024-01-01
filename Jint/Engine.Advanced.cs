24b45d6f851797f87f03d31a8874dc45a3176c40
Marko Lahma <marko.lahma@gmail.com>
2024-01-01 10:03:24 +0200

    Add Engine.Advanced.StackTrace to get current stack trace string (#1712)

--- Jint/Engine.Advanced.cs
+++ Jint/Engine.Advanced.cs
@@ -15,6 +15,23 @@ public class AdvancedOperations
     }
 
     /// <summary>
+    /// Gets current stack trace that is active in engine.
+    /// </summary>
+    public string StackTrace
+    {
+        get
+        {
+            var lastSyntaxElement = _engine._lastSyntaxElement;
+            if (lastSyntaxElement is null)
+            {
+                return string.Empty;
+            }
+
+            return _engine.CallStack.BuildCallStackString(lastSyntaxElement.Location);
+        }
+    }
+
+    /// <summary>
     /// Forcefully processes the current task queues (micro and regular), this API may break and change behavior!
     /// </summary>
     public void ProcessTasks()
