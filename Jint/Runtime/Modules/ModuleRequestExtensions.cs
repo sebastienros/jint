44f1b2e5936330a80846f357d9643e4dfd00458e
tomatosalat0 <david.huettig@gmx.net>
2024-01-08 21:39:16 +0100

    Refactoring for custom module loader support

---
+++ Jint/Runtime/Modules/ModuleRequestExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Jint.Runtime.Modules;
+
+public static class ModuleRequestExtensions
+{
+    /// <summary>
+    /// Returns true if the provided <paramref name="request"/>
+    /// is a json module, otherwise false.
+    /// </summary>
+    /// <example>
+    /// The following JavaScript import statement imports a JSON module
+    /// for which this method would return true.
+    /// <code>
+    /// import value from 'config.json' with { type: 'json' }
+    /// </code>
+    /// </example>
+    public static bool IsJsonModule(this ModuleRequest request)
+    {
+        return request.Attributes != null
+            && Array.Exists(request.Attributes, x => string.Equals(x.Key, "type", StringComparison.Ordinal) && string.Equals(x.Value, "json", StringComparison.Ordinal));
+    }
+}
