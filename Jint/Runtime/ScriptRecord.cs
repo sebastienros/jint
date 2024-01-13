a337f486e3ff4e59772a1126650b10c9eee14883
tomatosalat0 <101592647+tomatosalat0@users.noreply.github.com>
2024-01-13 19:53:19 +0100

    Script location should be passed to module resolver (#1740)

--- Jint/Runtime/ScriptRecord.cs
+++ Jint/Runtime/ScriptRecord.cs
@@ -2,4 +2,4 @@ using Esprima.Ast;
 
 namespace Jint.Runtime;
 
-internal sealed record ScriptRecord(Realm Realm, Script EcmaScriptCode, string Location) : IScriptOrModule;
+internal sealed record ScriptRecord(Realm Realm, Script EcmaScriptCode, string? Location) : IScriptOrModule;
