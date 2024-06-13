00d0727da120074397530d0688582388c689ebdd
Marko Lahma <marko.lahma@gmail.com>
2024-06-13 09:05:15 +0300

    Fix BigInt arithmetic assignment

--- Jint/Runtime/Interpreter/Expressions/JintBinaryExpression.cs
+++ Jint/Runtime/Interpreter/Expressions/JintBinaryExpression.cs
@@ -355,6 +355,7 @@ namespace Jint.Runtime.Interpreter.Expressions
                 }
                 else
                 {
+                    JintBinaryExpression.AssertValidBigIntArithmeticOperands(left, right);
                     number = JsBigInt.Create(TypeConverter.ToBigInt(left) - TypeConverter.ToBigInt(right));
                 }
 
