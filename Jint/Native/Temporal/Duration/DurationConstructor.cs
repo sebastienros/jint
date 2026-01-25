db19d654a2d24d6b1876145d1e122ec47c55284f
Marko Lahma <marko.lahma@gmail.com>
2026-01-25 10:02:35 +0200

    Implement Temporal.Duration (#2263)

--- Jint/Native/Temporal/Duration/DurationConstructor.cs
+++ Jint/Native/Temporal/Duration/DurationConstructor.cs
@@ -43,22 +43,44 @@ internal sealed class DurationConstructor : Constructor
     /// <summary>
     /// https://tc39.es/proposal-temporal/#sec-temporal.duration.from
     /// </summary>
-    private JsValue From(JsValue thisObject, JsCallArguments arguments)
+    private JsDuration From(JsValue thisObject, JsCallArguments arguments)
     {
         var item = arguments.At(0);
-        // TODO: Implement ToTemporalDuration
-        Throw.TypeError(_realm, "Temporal.Duration.from is not yet implemented");
-        return Undefined;
+        return ToTemporalDuration(item);
     }
 
     /// <summary>
     /// https://tc39.es/proposal-temporal/#sec-temporal.duration.compare
     /// </summary>
-    private JsValue Compare(JsValue thisObject, JsCallArguments arguments)
+    private JsNumber Compare(JsValue thisObject, JsCallArguments arguments)
     {
-        // TODO: Implement Duration comparison
-        Throw.TypeError(_realm, "Temporal.Duration.compare is not yet implemented");
-        return Undefined;
+        var one = ToTemporalDuration(arguments.At(0));
+        var two = ToTemporalDuration(arguments.At(1));
+
+        // For durations without calendar units, we can compare directly
+        // by converting to total nanoseconds
+        var d1 = one.DurationRecord;
+        var d2 = two.DurationRecord;
+
+        // Check if either duration has calendar units (years, months, weeks)
+        if (d1.Years != 0 || d1.Months != 0 || d1.Weeks != 0 ||
+            d2.Years != 0 || d2.Months != 0 || d2.Weeks != 0)
+        {
+            // Comparison with calendar units requires a relativeTo argument
+            var options = arguments.At(2);
+            if (options.IsUndefined() || options.IsNull())
+            {
+                Throw.RangeError(_realm, "A relativeTo option is required for comparing durations with calendar units");
+            }
+            // TODO: Implement relativeTo comparison
+            Throw.TypeError(_realm, "Duration comparison with calendar units is not yet fully implemented");
+        }
+
+        // Compare time-only durations
+        var ns1 = TemporalHelpers.TotalDurationNanoseconds(d1);
+        var ns2 = TemporalHelpers.TotalDurationNanoseconds(d2);
+
+        return JsNumber.Create(ns1.CompareTo(ns2));
     }
 
     protected internal override JsValue Call(JsValue thisObject, JsCallArguments arguments)
@@ -67,11 +89,143 @@ internal sealed class DurationConstructor : Constructor
         return Undefined;
     }
 
+    /// <summary>
+    /// https://tc39.es/proposal-temporal/#sec-temporal.duration
+    /// </summary>
     public override ObjectInstance Construct(JsCallArguments arguments, JsValue newTarget)
     {
-        // TODO: Implement Duration construction
-        Throw.TypeError(_realm, "Temporal.Duration constructor is not yet implemented");
+        var years = ToIntegerIfIntegral(arguments.At(0), "years");
+        var months = ToIntegerIfIntegral(arguments.At(1), "months");
+        var weeks = ToIntegerIfIntegral(arguments.At(2), "weeks");
+        var days = ToIntegerIfIntegral(arguments.At(3), "days");
+        var hours = ToIntegerIfIntegral(arguments.At(4), "hours");
+        var minutes = ToIntegerIfIntegral(arguments.At(5), "minutes");
+        var seconds = ToIntegerIfIntegral(arguments.At(6), "seconds");
+        var milliseconds = ToIntegerIfIntegral(arguments.At(7), "milliseconds");
+        var microseconds = ToIntegerIfIntegral(arguments.At(8), "microseconds");
+        var nanoseconds = ToIntegerIfIntegral(arguments.At(9), "nanoseconds");
+
+        var record = new DurationRecord(years, months, weeks, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds);
+
+        if (!TemporalHelpers.IsValidDuration(record))
+        {
+            Throw.RangeError(_realm, "Invalid duration");
+        }
+
+        return Construct(record);
+    }
+
+    private double ToIntegerIfIntegral(JsValue value, string name)
+    {
+        if (value.IsUndefined())
+            return 0;
+
+        var number = TypeConverter.ToNumber(value);
+
+        if (double.IsNaN(number))
+        {
+            Throw.RangeError(_realm, $"Duration {name} must be a finite number");
+        }
+
+        if (double.IsInfinity(number))
+        {
+            Throw.RangeError(_realm, $"Duration {name} must be a finite number");
+        }
+
+        if (number != System.Math.Truncate(number))
+        {
+            Throw.RangeError(_realm, $"Duration {name} must be an integer");
+        }
+
+        return number;
+    }
+
+    /// <summary>
+    /// https://tc39.es/proposal-temporal/#sec-temporal-totemporalduration
+    /// </summary>
+    internal JsDuration ToTemporalDuration(JsValue item)
+    {
+        if (item is JsDuration duration)
+        {
+            return duration;
+        }
+
+        if (item.IsString())
+        {
+            var parsed = TemporalHelpers.ParseDuration(item.ToString());
+            if (parsed is null)
+            {
+                Throw.RangeError(_realm, "Invalid duration string");
+            }
+            return Construct(parsed.Value);
+        }
+
+        if (item.IsObject())
+        {
+            var obj = item.AsObject();
+            var record = ToDurationRecord(obj);
+            if (!TemporalHelpers.IsValidDuration(record))
+            {
+                Throw.RangeError(_realm, "Invalid duration");
+            }
+            return Construct(record);
+        }
+
+        Throw.TypeError(_realm, "Invalid duration");
         return null!;
+    }
+
+    private DurationRecord ToDurationRecord(ObjectInstance obj)
+    {
+        var years = GetDurationProperty(obj, "years");
+        var months = GetDurationProperty(obj, "months");
+        var weeks = GetDurationProperty(obj, "weeks");
+        var days = GetDurationProperty(obj, "days");
+        var hours = GetDurationProperty(obj, "hours");
+        var minutes = GetDurationProperty(obj, "minutes");
+        var seconds = GetDurationProperty(obj, "seconds");
+        var milliseconds = GetDurationProperty(obj, "milliseconds");
+        var microseconds = GetDurationProperty(obj, "microseconds");
+        var nanoseconds = GetDurationProperty(obj, "nanoseconds");
+
+        // At least one property must be defined
+        if (years == 0 && months == 0 && weeks == 0 && days == 0 &&
+            hours == 0 && minutes == 0 && seconds == 0 &&
+            milliseconds == 0 && microseconds == 0 && nanoseconds == 0)
+        {
+            // Check if any property was actually present
+            var hasAny = obj.HasProperty("years") || obj.HasProperty("months") ||
+                         obj.HasProperty("weeks") || obj.HasProperty("days") ||
+                         obj.HasProperty("hours") || obj.HasProperty("minutes") ||
+                         obj.HasProperty("seconds") || obj.HasProperty("milliseconds") ||
+                         obj.HasProperty("microseconds") || obj.HasProperty("nanoseconds");
+            if (!hasAny)
+            {
+                Throw.TypeError(_realm, "Duration object must have at least one temporal property");
+            }
+        }
+
+        return new DurationRecord(years, months, weeks, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds);
+    }
+
+    private double GetDurationProperty(ObjectInstance obj, string name)
+    {
+        var value = obj.Get(name);
+        if (value.IsUndefined())
+            return 0;
+
+        var number = TypeConverter.ToNumber(value);
+        if (double.IsNaN(number) || double.IsInfinity(number))
+        {
+            Throw.RangeError(_realm, $"Duration {name} must be a finite number");
+        }
+
+        if (number != System.Math.Truncate(number))
+        {
+            Throw.RangeError(_realm, $"Duration {name} must be an integer");
+        }
+
+        return number;
     }
 
     internal JsDuration Construct(DurationRecord duration)
